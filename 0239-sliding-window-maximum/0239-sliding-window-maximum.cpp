4217699c95b2d3fc2225a4df348e7e67923c4415
AbdelrahmanJaber <113253216+AbdelrahmanJaber@users.noreply.github.com>
2023-02-18 00:41:47 +0200

    Time: 317 ms (57.3%), Space: 134.7 MB (83.93%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,29 +1,40 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        // priority_queue<int> pq;
-        multiset<int> pq;
+        // Monotonic decreasing deque
+        deque<int> dq;
         vector<int> result;
 
         int i = 0;
-
-        for(; i < k && i < nums.size(); i++){
-            pq.insert(nums[i]);
+        // initialize the first k elements
+        for(; i < k && i < nums.size() ; i++){
+            while(!dq.empty() && nums[i] > dq.back()){
+                dq.pop_back();
+            }
+            
+            dq.push_back(nums[i]);
         }
-
-        result.push_back(*pq.rbegin());
-
+        result.push_back(dq.front());
 
         while(i < nums.size()){
-            pq.insert(nums[i]); 
+            // remove the element from the window
+            if(dq.front() == nums[i-k]){
+                dq.pop_front();
+            }
 
-            auto it = pq.find(nums[i-k]);
-            pq.erase(it);
+            while(!dq.empty() && nums[i] > dq.back()){
+                dq.pop_back();
+            }
+            
+            dq.push_back(nums[i]);
+            result.push_back(dq.front());
 
-            result.push_back(*pq.rbegin());
             i++;
         }
 
         return result;
     }
 };
+
+// Time complexity = O(n), where n: nums.size()
+// Space complexity = O(n) [ O(k) for deque + O(n - k + 1) for result array ]
