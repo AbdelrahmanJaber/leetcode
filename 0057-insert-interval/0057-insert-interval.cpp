be7a26fd025050af12df90c62e88c3007a53ef73
AbdelrahmanJaber <113253216+AbdelrahmanJaber@users.noreply.github.com>
2023-02-19 01:16:12 +0200

    Time: 24 ms (27.16%), Space: 17.7 MB (8.43%) - LeetHub

---
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+    bool isTwoIntervalsOverlabbed(vector<int>& interval1, vector<int>& interval2){
+        return min(interval1[1], interval2[1]) - max(interval1[0], interval2[0]) >= 0;
+    }
+
+    vector<int> mergeTwoIntervals(vector<int>& interval1, vector<int>& interval2){
+        return {min(interval1[0], interval2[0]), max(interval1[1], interval2[1])};
+    }
+
+    vector<vector<int>> mergeIntervals(vector<vector<int>>& intervals){
+        vector<vector<int>> result;
+
+        vector<int> currentInterval;
+        for(int i = 0 ; i < intervals.size() ; i++){
+            currentInterval = {intervals[i][0], intervals[i][1]};
+
+            while(i < intervals.size() && isTwoIntervalsOverlabbed(currentInterval, intervals[i])){
+                currentInterval = mergeTwoIntervals(currentInterval, intervals[i]);
+                i++;
+            }
+
+            i--;
+            result.push_back(currentInterval);
+        }
+
+        return result;
+    }
+
+    static bool comp(const vector<int>& vec1, const vector<int>& vec2){
+        return vec1[0] < vec2[0];
+    }
+
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        bool newIntervalAdded = false;
+
+        // insert the new interval to the list of intervals while keeping the order in ascending order
+        // linear search until I find the first start interval which is greater than new Interval start
+        for(int i = 0 ; i < intervals.size() ; i++){
+            if(intervals[i][0] > newInterval[0]){
+                // update the flag
+                newIntervalAdded = true;
+
+                // insert the new interval in the current position and shift the remaining intervals
+                intervals.insert(intervals.begin() + i, newInterval);
+                break;
+            }
+        }
+
+        if(!newIntervalAdded){
+            intervals.push_back(newInterval);
+        }
+
+        return mergeIntervals(intervals);
+    }
+};
+
+// Time complexity = O(n), where n: number of intervals 
+// (due to: 1-linear search O(n) //  2-insert and shift O(n) //  3- mergeIntervals O(n) )
+
+// Space complexity = O(1) if we ignore the result space (if we don't O(n))
