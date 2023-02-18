fb6049b4ab4300516197d0e096f7d678397a99fa
AbdelrahmanJaber <113253216+AbdelrahmanJaber@users.noreply.github.com>
2023-02-19 00:50:48 +0200

    Time: 35 ms (91.79%), Space: 19.9 MB (36.71%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,41 +1,42 @@
 class Solution {
 public:
+    bool isTwoIntervalsOverlap(vector<int>& interval1, vector<int>& interval2){
+        return min(interval1[1], interval2[1]) - max(interval1[0], interval2[0]) >= 0;
+    }
+
+    vector<int> mergeIntervals(vector<int>& interval1, vector<int>& interval2){
+        return {min(interval1[0], interval2[0]), max(interval1[1], interval2[1])};
+    }
+
     static bool comp(const vector<int>& vec1, const vector<int>& vec2){
         return vec1[0] < vec2[0];
     }
 
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        // sort the intervals
+        // don't forget to sort the intervals
         sort(intervals.begin(), intervals.end(), comp);
 
         vector<vector<int>> result;
-    
-        int start = intervals[0][0];
-        int end = intervals[0][1];
 
-        for(int i = 1 ; i < intervals.size() ; i++){
-            // check overlapping
-            if(intervals[i][0] <= end){
-                start = min(intervals[i][0], start);
-                end = max(intervals[i][1], end);
-            }
-            // non overlapping
-            else{
-                // push the interval to the result
-                result.push_back({start, end});
+        vector<int> currentInterval;
+        for(int i = 0 ; i < intervals.size() ; i++){
+            currentInterval = {intervals[i][0], intervals[i][1]};
 
-                // update the start and end
-                start = intervals[i][0];
-                end = intervals[i][1];
+            while(i < intervals.size() && isTwoIntervalsOverlap(currentInterval, intervals[i])){
+                currentInterval = mergeIntervals(currentInterval, intervals[i]);
+                i++;
             }
+
+            i--;
+            result.push_back(currentInterval);
         }
-
-        // push the last interval
-        result.push_back({start, end});
 
         return result;
     }
 };
 
-// Time complexity = O(n), where n: number of intervals
-// Space complexity = O(n) because of the result array (O(1) if we don't consider the result space complexity)
+// Time complexity = O(n)
+// Space complexity = O(1) iif we ignore the result space
+
+// NOTE: solve this question in the start, end way (easier)
+// But the functions and the way here is too useful
