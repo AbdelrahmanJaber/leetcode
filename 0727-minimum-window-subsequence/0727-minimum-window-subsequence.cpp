5692eea15ee646d482c929b10a37f22b92268c0d
AbdelrahmanJaber <113253216+AbdelrahmanJaber@users.noreply.github.com>
2023-02-18 02:00:55 +0200

    Time: 17 ms (76.36%), Space: 8.2 MB (78.18%) - LeetHub

---
+++ 0727-minimum-window-subsequence/0727-minimum-window-subsequence.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    string minWindow(string s1, string s2) {
+        int s1_index = 0;
+        int s2_index = 0;
+
+        int start = 0;
+        int end = 0;
+
+        int min_length = INT_MAX;
+        string substring = "";
+
+        while(s1_index < s1.size()){
+
+            // try to find the subsequence
+            if(s1[s1_index] == s2[s2_index]){
+                s2_index++;
+            }
+
+            if(s2_index == s2.size()){
+                end = s1_index;
+                start = s1_index;
+                s2_index--;
+
+                while(s2_index >= 0){
+                    if(s1[start] == s2[s2_index]) s2_index--;
+                    start--;
+                }
+
+                if(end - start < min_length){
+                    min_length = end - start;
+                    substring = s1.substr(start + 1, end - start);
+                }
+                
+                s2_index = 0;
+                s1_index = start+2;
+            }
+
+            s1_index++;
+        }
+
+        return substring;
+    }
+};
+
+// Time complexity = O(n + m), where n: s1 size() and m: s2 size()
+// Space complexity = O(n) due to result string, in the worst case if the result substring is the whole s1 (ex: s1 = abbbbf, s2 = af)
