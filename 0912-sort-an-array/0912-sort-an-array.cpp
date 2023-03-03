aaf2dc60d386b378d86ff585f93442be28471e78
AbdelrahmanJaber <113253216+AbdelrahmanJaber@users.noreply.github.com>
2023-03-03 21:58:19 +0200

    Time: 143 ms (73.51%), Space: 61.3 MB (82.53%) - LeetHub

---
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        return nums;
+    }
+};
+
+// you should implement the sort by your hand, not like this
