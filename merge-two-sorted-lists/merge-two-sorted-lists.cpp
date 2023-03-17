4207200c7cd31c5f9e68e182fe0599e71f188262
AbdelrahmanJaber <113253216+AbdelrahmanJaber@users.noreply.github.com>
2023-03-17 16:00:54 +0200

    Time: 12 ms, Memory: 14.8 MB - LeetHub

---
+++ merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -0,0 +1,44 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+
+ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+    ListNode* prehead = new ListNode(-1);
+    ListNode* prev = prehead;
+    
+    while(list1 != NULL && list2 != NULL){
+        if(list1 -> val < list2 -> val){
+            prev -> next = list1;
+            list1 = list1 -> next;
+        }
+        else{
+            prev -> next = list2;
+            list2 = list2 -> next;
+        }
+
+        prev = prev -> next;
+    }
+    
+    if(list1 != NULL){
+        prev -> next = list1;
+    }
+    else if(list2 != NULL){
+        prev -> next = list2;
+    }
+    
+    return prehead -> next;
+}
+};
+
+/* Time complecity = O(n + m) where n: number of nodes in list1, m: number of nodes in list2
+   Space complexity = O(1)
+   */
