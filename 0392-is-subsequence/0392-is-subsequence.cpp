320767efcd3d6e39ea8873fe1ae34f1cda8686eb
AbdelrahmanJaber <113253216+AbdelrahmanJaber@users.noreply.github.com>
2023-10-05 20:37:08 +0300

    Time: 0 ms (100%), Space: 6.8 MB (17.97%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -1,14 +1,20 @@
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
-        int s_ptr = 0;
+        int s_ptr = 0; // O(1)
 
         // loop through t_string
-        for(int i = 0 ; i < t.size() ; i++){
+        for(int i = 0 ; i < t.size() ; i++){ // O(t), where t: size of string t
             // I have found the character in t, so increment the s_ptr
             if(s[s_ptr] == t[i]){
                 s_ptr++;
             }
+
+             
+            if(s_ptr == s.size()){
+                return true;
+            }
+
         }
 
         // if(s_ptr == s.size()){
@@ -19,6 +25,9 @@ public:
         // }
 
         // check if the s_ptr has reached the end of the string s
-        return (s_ptr == s.size()) ? true : false;
+        return s_ptr == s.size(); // O(1)
     }
 };
+
+// Time complexity = O(t), where t: size of string t
+// Space complexity = O(1)
