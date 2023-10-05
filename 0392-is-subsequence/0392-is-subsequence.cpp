3799c6c9fa17a87e44f3ec027f429b80862423d0
AbdelrahmanJaber <113253216+AbdelrahmanJaber@users.noreply.github.com>
2023-10-05 20:30:04 +0300

    Time: 0 ms (100%), Space: 6.7 MB (31.84%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -1,14 +1,24 @@
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
-        int s_pointer = 0;
+        int s_ptr = 0;
 
+        // loop through t_string
         for(int i = 0 ; i < t.size() ; i++){
-            if(s[s_pointer] == t[i]){
-                s_pointer++;
+            // I have found the character in t, so increment the s_ptr
+            if(s[s_ptr] == t[i]){
+                s_ptr++;
             }
         }
 
-        return (s_pointer == s.size()) ? true : false;
+        // if(s_ptr == s.size()){
+        //     return true;
+        // }
+        // else{
+        //     return false;
+        // }
+
+        // check if the s_ptr has reached the end of the string s
+        return (s_ptr == s.size()) ? true : false;
     }
 };
